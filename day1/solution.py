e6ab620a9cff1e3bd33e85379b72c23ab103a4d7
Maina Kamau <lewiikamaa8@gmail.com>
2022-08-27 14:52:30 +0300

    remove commented code
    Signed-off-by: Maina Kamau <lewiikamaa8@gmail.com>

--- day1/solution.py
+++ day1/solution.py
@@ -4,18 +4,8 @@ from pathlib import Path
 def solution():
     filepath = Path("inputdata.txt")
     data = filepath.read_text().splitlines()
-    # data = [199,
-    #         200,
-    #         208,
-    #         210,
-    #         200,
-    #         207,
-    #         240,
-    #         269,
-    #         260,
-    #         263]
     counter = 0
     for idx in range(1, len(data)):
         counter += 1 if int(data[idx]) > int(data[idx - 1]) else 0
-    print(counter)
+    return counter
 solution()
