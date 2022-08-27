8b0875834e087148c58c98fdc25466009ce4c9b7
Maina Kamau <lewiikamaa8@gmail.com>
2022-08-27 15:57:03 +0300

    complete day 1 of AOC
    Signed-off-by: Maina Kamau <lewiikamaa8@gmail.com>

--- day1/solution.py
+++ day1/solution.py
@@ -1,11 +1,25 @@
-from pathlib import Path 
+from pathlib import Path
 
 
-def solution():
-    filepath = Path("inputdata.txt")
-    data = filepath.read_text().splitlines()
-    counter = 0
-    for idx in range(1, len(data)):
-        counter += 1 if int(data[idx]) > int(data[idx - 1]) else 0
-    return counter
-solution()
+class DayOneSolution:
+
+    def __init__(self) -> None:
+        self.filepath = Path("inputdata.txt")
+        self.data = self.filepath.read_text().splitlines()
+        self.counter = 0
+
+    def part1(self) -> int:
+        for idx in range(1, len(self.data)):
+            self.counter += 1 if int(self.data[idx]) > int(self.data[idx - 1]) else 0
+        return self.counter
+
+    def part2(self) -> int:
+        for idx in range(0, len(self.data)):
+            self.counter += 1 if sum(list(map(lambda x: int(x), self.data[idx:idx + 3]))) < sum(
+                list(map(lambda x: int(x), self.data[idx + 1:idx + 4]))) else 0
+        return self.counter
+
+
+if __name__ == "__main__":
+    inst = DayOneSolution()
+    print(inst.part2())
